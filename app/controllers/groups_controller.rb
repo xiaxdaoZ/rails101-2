565844a01077fa0a2366ea83cf08ce1a9aa2028b
xiaxdaoZ <zz128144@gmail.com>
2017-04-15 21:46:45 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
-  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy, :join, :quit]
 
   def index
     @groups = Group.all
@@ -48,6 +48,32 @@ class GroupsController < ApplicationController
       redirect_to groups_path
     end
 
+    def join
+      @group = Group.find(params[:id])
+
+      if !current_user.is_member_of?(@group)
+        current_user.join!(@group)
+        flash[:notice] = "加入本讨论版成功！"
+      else
+        flash[:warning] = "你已经是本讨论版成员了！"
+      end
+
+      redirect_to group_path(@group)
+    end
+
+    def quit
+      @group = Group.find(params[:id])
+
+      if current_user.is_member_of?(@group)
+        current_user.quit!(@group)
+        flash[:alert] = "已退出来讨论版！"
+      else
+        flash[:warning] = "你不是本讨论版成员，怎么退出 ＸＤ"
+      end
+
+      redirect_to group_path(@group)
+    end
+
   private
 
   def group_params
@@ -55,7 +81,7 @@ class GroupsController < ApplicationController
   end
 
   def find_group_and_check_permission
-    @group = Gruop.find(params[:id])
+    @group = Group.find(params[:id])
 
     if current_user != @group.user
       redirect_to root_path, alert: "You have no permission."
